c72522913ae184770415f76058984ef06c38fa69
Brandon Brule <bbrule@cbnco.com>
2017-02-22 09:05:01 -0500

    firefox spacing

--- js/its-calc.js
+++ js/its-calc.js
@@ -116,7 +116,7 @@
       'font-family:arial, helvetica;',
       'background: none;',
       'border:none;',
-      'width: 214px;',
+      'width: 213px;',
       'height: 40px;',
       'color:#fff;',
       'padding-left:10px;',
@@ -177,7 +177,7 @@
 
       '.its-calc .its-calc-submit-container{',
       'float:right;',
-      'width: 136px;',
+      'width: 147px;',
       'padding: 7px 0 0 0;',
       'margin-right: 10px;',
       '}',
